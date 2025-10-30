453a132a43d01701564567d085cb787395b4af8a
Emanuel Bodin <bodinemanuel@gmail.com>
2025-10-30 23:55:26 +0100

    added build command

---
+++ server/dist/src/server.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vite_express_1 = __importDefault(require("vite-express"));
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+vite_express_1.default.config({
+    inlineViteConfig: {
+        base: '',
+        root: 'client',
+        build: { outDir: '../build' },
+    },
+});
+vite_express_1.default.listen(app, 3000, () => console.info('Server is listening (3000)...'));
